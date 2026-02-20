f5f92a671a4b4b5ace6fdb4edfd063c47d0e822e
SoumyaSharif <SoumyaSharifCapgemini@outlook.com>
2026-02-21 00:00:54 +0530

    [Soumya] : modified files

--- FeetMeasurement/src/main/java/com/quantityMeasurementApp/QuantityMeasurementApp.java
+++ FeetMeasurement/src/main/java/com/quantityMeasurementApp/QuantityMeasurementApp.java
@@ -6,7 +6,11 @@ public class QuantityMeasurementApp {
 
 		Feet f1 = new Feet(1.0);
 		Feet f2 = new Feet(1.0);
+		
+		Inches n1 = new Inches(1.0);
+		Inches n2 = new Inches(1.0);
 
 		System.out.println("Equal: " + f1.equals(f2));
+		System.out.println("Inches equal: " + n1.equals(n2));
 	}
 }
